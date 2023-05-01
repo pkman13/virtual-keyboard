70898ea722bd109f59acf7b7710728b4454a5d59
Sergey <sergey.grinkevich@gmail.com>
2023-05-02 00:14:34 +0300

    feat: add leaters

--- js/keybord.js
+++ js/keybord.js
@@ -5,80 +5,80 @@ export default `
 <main>
     <section class="keyboard">
         <div class="row">
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key delet" data-code="key"><span></span></div>
+            <div class="key" data-code="Escape"><span>esc</span></div>
+            <div class="key" data-code="Digit1"><span>1</span></div>
+            <div class="key" data-code="Digit2"><span>2</span></div>
+            <div class="key" data-code="Digit3"><span>3</span></div>
+            <div class="key" data-code="Digit4"><span>4</span></div>
+            <div class="key" data-code="Digit5"><span>5</span></div>
+            <div class="key" data-code="Digit6"><span>6</span></div>
+            <div class="key" data-code="Digit7"><span>7</span></div>
+            <div class="key" data-code="Digit8"><span>8</span></div>
+            <div class="key" data-code="Digit9"><span>9</span></div>
+            <div class="key" data-code="Digit0"><span>0</span></div>
+            <div class="key" data-code="Minus"><span>-</span></div>
+            <div class="key" data-code="Equal"><span>=</span></div>
+            <div class="key delet" data-code="Delet"><span>delet</span></div>
         </div>
         <div class="row">
-            <div class="key tab" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
+            <div class="key tab" data-code="key"><span>tab</span></div>
+            <div class="key" data-code="key"><span>q</span></div>
+            <div class="key" data-code="key"><span>w</span></div>
+            <div class="key" data-code="key"><span>e</span></div>
+            <div class="key" data-code="key"><span>r</span></div>
+            <div class="key" data-code="key"><span>t</span></div>
+            <div class="key" data-code="key"><span>y</span></div>
+            <div class="key" data-code="key"><span>u</span></div>
+            <div class="key" data-code="key"><span>i</span></div>
+            <div class="key" data-code="key"><span>o</span></div>
+            <div class="key" data-code="key"><span>p</span></div>
+            <div class="key" data-code="key"><span>[</span></div>
+            <div class="key" data-code="key"><span>]</span></div>
+            <div class="key" data-code="key"><span>\\</span></div>
         </div>
         <div class="row">
-            <div class="key caps-lock" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key enter" data-code="key"><span></span></div>
+            <div class="key caps-lock" data-code="key"><span>caps lock</span></div>
+            <div class="key" data-code="key"><span>a</span></div>
+            <div class="key" data-code="key"><span>s</span></div>
+            <div class="key" data-code="key"><span>d</span></div>
+            <div class="key" data-code="key"><span>f</span></div>
+            <div class="key" data-code="key"><span>g</span></div>
+            <div class="key" data-code="key"><span>h</span></div>
+            <div class="key" data-code="key"><span>j</span></div>
+            <div class="key" data-code="key"><span>k</span></div>
+            <div class="key" data-code="key"><span>l</span></div>
+            <div class="key" data-code="key"><span>;</span></div>
+            <div class="key" data-code="key"><span>'</span></div>
+            <div class="key enter" data-code="key"><span>return</span></div>
         </div>
         <div class="row">
-            <div class="key shift" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key" data-code="key"><span></span></div>
-            <div class="key shift" data-code="key"><span></span></div>
+            <div class="key shift" data-code="key"><span>shift</span></div>
+            <div class="key" data-code="key"><span>z</span>z</div>
+            <div class="key" data-code="key"><span>x</span>x</div>
+            <div class="key" data-code="key"><span>c</span>c</div>
+            <div class="key" data-code="key"><span>v</span>v</div>
+            <div class="key" data-code="key"><span>b</span>b</div>
+            <div class="key" data-code="key"><span>n</span>n</div>
+            <div class="key" data-code="key"><span>m</span>m</div>
+            <div class="key" data-code="key"><span>,</span>,</div>
+            <div class="key" data-code="key"><span>.</span>.</div>
+            <div class="key" data-code="key"><span>/</span>/</div>
+            <div class="key shift" data-code="key"><span>shift</span></div>
         </div>
         <div class="row">
-            <div class="key row-5" data-code="key"><span></span></div>
-            <div class="key row-5" data-code="key"><span></span></div>
-            <div class="key row-5" data-code="key"><span></span></div>
-            <div class="key command" data-code="key"><span></span></div>
+            <div class="key row-5" data-code="key"><span>lng</span></div>
+            <div class="key row-5" data-code="key"><span>control</span></div>
+            <div class="key row-5" data-code="key"><span>option</span></div>
+            <div class="key command" data-code="key"><span>command</span></div>
             <div class="key space" data-code="key"><span></span></div>
-            <div class="key command" data-code="key"><span></span></div>
-            <div class="key  row-5" data-code="key"><span></span></div>
-            <div class="key move left" data-code="key"><span></span></div>
+            <div class="key command" data-code="key"><span>command</span></div>
+            <div class="key  row-5" data-code="key"><span>option</span></div>
+            <div class="key move left" data-code="key"><span>◀ </span></div>
             <div class="move">
-                <div class="key move" data-code="key"><span></span></div>
-                <div class="key move" data-code="key"><span></span></div>
+                <div class="key move" data-code="key"><span>▲</span></div>
+                <div class="key move" data-code="key"><span>▼</span></div>
             </div>
-            <div class="key move right" data-code="key"><span></span></div>
+            <div class="key move right" data-code="key"><span>▶</span></div>
         </div>
 
     </section>
